3c43377eeaddd17502fdbdfaeed3eac49d0891a5
unknown <facubazan992009@gmail.com>
2025-06-24 18:56:13 -0300

    Comenzado con vista carrito

--- fronted/src/pages/Carrito.jsx
+++ fronted/src/pages/Carrito.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const Carrito = () => {
   return (
-    <div>Carrito</div>
+    <div>
+      <Footer/>
+    </div>
   )
 }
 
