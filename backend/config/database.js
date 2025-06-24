e999fd81acdf29b0f7d4b1e3304cb660408a4619
unknown <facubazan992009@gmail.com>
2025-06-24 18:47:13 -0300

    Empezando vista de producto

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2');
 const conection = mysql2.createConnection({
     host: "localhost",
     user: "root",
-    password: "pixel2025",
+    password: "frida2023",
     database: "pixel_salud"
 });
 
