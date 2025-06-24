f0c7f87fd4a855883ffdbb4142a8042de2243fd5
unknown <facubazan992009@gmail.com>
2025-06-24 19:06:43 -0300

    Agrego logica para encontrar el id de Cliente

---
+++ fronted/src/store/useClienteStore.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getCliente = async () =>{
+    try {
+      const usuariosResponse = await axios.get("http://localhost:5000/clientes");
+      const usuarios = usuariosResponse.data;
+      const usuarioLogueado = usuarios.find((user) => user.logueado === 1);
+
+      if (!usuarioLogueado) {
+        console.log("Usuario no encontrado o logueado.");
+        return;
+      }
+
+      const idCliente = usuarioLogueado.idCliente;
+      return idCliente
+    } catch (error) {
+        console.error("Usuario no encontrado o logueado.")
+    }
+}
