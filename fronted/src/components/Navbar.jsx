d58bfdfcd16784b24a89035654460809bb0b795a
unknown <facubazan992009@gmail.com>
2025-06-28 14:06:37 -0300

    actulizando carrito

--- fronted/src/components/Navbar.jsx
+++ fronted/src/components/Navbar.jsx
@@ -85,13 +85,13 @@ const Navbar = () => {
         </div>
         <Link to="/carrito" className="relative">
           <img src={cartIcon} className="w-5 min-w-5" alt="cartIcon" />
-        {/*   <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-primary-700 text-white aspect-square rounded-full text-[8px]">
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-primary-700 text-white aspect-square rounded-full text-[8px]">
             {totalItems > 0 && (
               <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-primary-700 text-white aspect-square rounded-full text-[8px]">
                 {totalItems}
               </p>
             )}
-          </p> */}
+          </p>
         </Link>
         <button
           onClick={() => setIsMenuOpen(true)}
