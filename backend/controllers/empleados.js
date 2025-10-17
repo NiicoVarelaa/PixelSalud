d8360616aac718a4af9b014ebbef717d381c0fa7
unknown <facubazan992009@gmail.com>
2025-10-16 22:22:19 -0300

    Modifico controlador de crearEmpleados y updateEmpleados

--- backend/controllers/empleados.js
+++ backend/controllers/empleados.js
@@ -1,48 +1,70 @@
-const { conection } = require("../config/database")
+const { conection } = require("../config/database");
+const bcryptjs = require("bcryptjs")
 
 const getEmpleados = (req, res) => {
-    const consulta = "select * from Empleados"
+  const consulta = "select * from Empleados";
 
-    conection.query(consulta, (err, results) =>{
-        if(err) throw err
-        res.json(results)
-    })
-}
+  conection.query(consulta, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+};
 
-const createEmpleado = (req, res) =>{
-    const {nombreEmpleado, emailEmpleado, contraEmpleado, rol} = req.body
+const createEmpleado = async (req, res) => {
+  const { nombreEmpleado, apellidoEmpleado, emailEmpleado, contraEmpleado } =req.body;
 
-    const consulta = "insert into empleados (nombreEmpleado, emailEmpleado, contraEmpleado, rol) values (?,?,?,?);"
+  let salt = await bcryptjs.genSalt(10);
+  let contraEncrip = await bcryptjs.hash(contraEmpleado, salt);
 
-    conection.query(consulta,[nombreEmpleado,emailEmpleado,contraEmpleado,rol],(err,result)=>{
+  const exist = "select * from empleados where emailEmpleado=?";
+
+  const clienteExist = await query(exist, [emailEmpleado]);
+  if (clienteExist[0]) {
+    return res
+      .status(409)
+      .json({ error: "El usuario que intentas crear, ya se encuentra creado" });
+  } else {
+    const consulta =
+      "insert into empleados (nombreEmpleado, apellidoEmpleado,emailEmpleado, contraEmpleado) values (?,?,?,?);";
+
+    conection.query(
+      consulta,
+      [nombreEmpleado, apellidoEmpleado, emailEmpleado, contraEncrip],
+      (err, result) => {
         if (err) {
-            console.error('Error al crear el empleado:', err);
-            return res.status(500).json({ error: 'Error al crear el empleado' });
+          console.error("Error al crear el empleado:", err);
+          return res.status(500).json({ error: "Error al crear el empleado" });
         }
-        res.status(201).json({ message: 'Empleado creado correctamente'});
-    })
+        res.status(201).json({ message: "Empleado creado correctamente" });
+      }
+    );
+  }
+};
 
-}
+const updateEmpleado =  async(req, res) => {
+  const { nombreEmpleado, apellidoEmpleado, emailEmpleado, contraEmpleado} = req.body;
+  const idEmpleado = req.params.idEmpleado;
+  let salt = await bcryptjs.genSalt(10);
+  let contraEncrip = await bcryptjs.hash(contraEmpleado, salt);
+  const consulta =
+    "update empleados set nombreEmpleado=?, apellidoEmpleado=?, emailEmpleado=?, contraEmpleado=? where idEmpleado=?";
 
-const updateEmpleado = (req, res) => {
-    
-    const {nombreEmpleado, emailEmpleado, contraEmpleado, rol} = req.body
-    const idEmpleado = req.params.idEmpleado
-
-    const consulta = "update empleados set nombreEmpleado=?, emailEmpleado=?, contraEmpleado=?, rol=? where idEmpleado=?"
-
-    conection.query(consulta, [nombreEmpleado, emailEmpleado, contraEmpleado, rol, idEmpleado], (err,results)=>{
-        if (err) {
-            console.error('Error al obtener el empleado:', err);
-            return res.status(500).json({ error: 'Error al actulizar el empleado' });
-        }
-        res.status(200).json(results);
-    })
-
-}
+  conection.query(
+    consulta,
+    [nombreEmpleado, apellidoEmpleado, emailEmpleado, contraEncrip, idEmpleado],
+    (err, results) => {
+      if (err) {
+        console.error("Error al obtener el empleado:", err);
+        return res
+          .status(500)
+          .json({ error: "Error al actulizar el empleado" });
+      }
+      res.status(200).json(results);
+    }
+  );
+};
 
 const actualizarLogueado = (req, res) => {
-  
   const id = req.params.idEmpleado;
 
   // Primero deslogueamos a todos
@@ -66,10 +88,9 @@ const actualizarLogueado = (req, res) => {
   });
 };
 
-
 const desloguearEmpleado = (req, res) => {
   const id = req.params.idEmpleado;
-  const consulta = `UPDATE Empleados SET logueado = 0 WHERE idEmpleado = ?`; 
+  const consulta = `UPDATE Empleados SET logueado = 0 WHERE idEmpleado = ?`;
 
   conection.query(consulta, [id], (err, results) => {
     if (err) {
@@ -83,19 +104,25 @@ const desloguearEmpleado = (req, res) => {
   });
 };
 
-
 const deleteEmpleado = (req, res) => {
-    const idEmpleado = req.params.idEmpleado
+  const idEmpleado = req.params.idEmpleado;
 
-    const consulta = "delete from Empleados where idEmpleado=?"
+  const consulta = "delete from Empleados where idEmpleado=?";
 
-    conection.query(consulta, [idEmpleado],(err,results)=>{
-        if (err) {
-            console.error('Error al obtener el empleado:', err);
-            return res.status(500).json({ error: 'Error al actualizar el empleado' });
-        }
-        res.status(200).json({ message: 'Empleado eliminado correctamente' });
-    })
-}
+  conection.query(consulta, [idEmpleado], (err, results) => {
+    if (err) {
+      console.error("Error al obtener el empleado:", err);
+      return res.status(500).json({ error: "Error al actualizar el empleado" });
+    }
+    res.status(200).json({ message: "Empleado eliminado correctamente" });
+  });
+};
 
-module.exports = {getEmpleados, createEmpleado, updateEmpleado, deleteEmpleado, actualizarLogueado, desloguearEmpleado} 
+module.exports = {
+  getEmpleados,
+  createEmpleado,
+  updateEmpleado,
+  deleteEmpleado,
+  actualizarLogueado,
+  desloguearEmpleado,
+};
