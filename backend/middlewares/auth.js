b8b98ebe65901d22d0ede9c99606905700776eef
unknown <facubazan992009@gmail.com>
2025-10-30 19:19:22 -0300

    Termino con la creacion de middlewares y empiezo a validar rutas con los mismos

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,1 +1,29 @@
 const jwt = require ("jsonwebtoken")
+
+const auth = async (req, res , next)=>{
+    try {
+        const token = req.header("auth")?.replace("Bearer", "")
+
+        if(!token){
+            return res.status(401).json({msg: "Token ausente, acceso denegado"})
+        }
+
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+
+        req.user = payload;
+        next();
+    } catch (error) {
+        console.error("Error en middleware auth:", error.name)
+
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({msg: "Token expirado"})
+        }
+        if (error.name === "JsonWebTokenError") {
+            return res.status(401).json({msg: "Token no valido"})
+        }
+
+        res.status(500).json({msg:"Server error al validar Token", error})
+    }
+}
+
+module.exports = auth;
