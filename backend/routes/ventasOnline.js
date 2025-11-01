e9ecf05d63331a9b3ef2cb59dbcd914221a4ba15
unknown <facubazan992009@gmail.com>
2025-11-01 20:19:48 -0300

    Agrego validacion y proteccion a las rutas de ventasOnline

--- backend/routes/ventasOnline.js
+++ backend/routes/ventasOnline.js
@@ -7,10 +7,13 @@ const {
 } = require("../controllers/ventasOnline");
 
 const router = express.Router();
+const auth = require("../middlewares/auth")
+const {verificarRol, verificarPermisos}= require("../middlewares/verificarPermisos")
 
-router.get("/ventaOnline/misCompras/:idCliente", mostrarCompras);
-router.get("/ventasOnline/todas", mostrarTodasLasVentas);
-router.post("/ventaOnline/crear", registrarVentaOnline);
+
+router.get("/ventaOnline/misCompras/:idCliente",auth,verificarRol(["admin","cliente"]), mostrarCompras);
+router.get("/ventasOnline/todas",auth,verificarRol(["admin","empleado"]),verificarPermisos("ver_ventasTotalesO"), mostrarTodasLasVentas);
+router.post("/ventaOnline/crear",auth,verificarRol(["cliente"]), registrarVentaOnline);
 router.put("/ventaOnline/estado", actualizarEstadoVenta);
 
 module.exports = router;
