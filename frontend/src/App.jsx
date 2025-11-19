741c728bc11152876a5337264773bba2d8ee3bca
unknown <facubazan992009@gmail.com>
2025-11-18 22:00:58 -0300

    Termino con panel medicos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,9 +27,6 @@ import AdminVentasO from "./components/AdminVentasO";
 import AdminClientes from "./components/AdminClientes";
 import AdminEmpleados from "./components/AdminEmpleados";
 import AdminMenu from "./components/AdminMenu";
-/* import MedicosMenu from "./components/MedicosMenu";
-import Receta from "./components/Receta";
-import CrearReceta from "./components/CrearReceta"; */
 import PanelMedicos from "./pages/PanelMedico";
 import MenuProductos from "./components/MenuProductos";
 import OpcionesProductos from "./components/OpcionesProductos";
@@ -102,13 +99,7 @@ const App = () => {
             </Route>
           </Route>
         </Route>
-
-{/*         <Route path="/PanelMedicos/*" element={<PanelMedicos />}>
-          <Route index element={<MedicosMenu />} />
-          <Route path="Receta" element={<Receta />} />
-          <Route path="CrearReceta" element={<CrearReceta />} />
-        </Route>
- */}
+        
         <Route element={<ProtectedRoute allowedRoles={["empleado"]} />}>
           <Route path="/panelempleados" element={<PanelEmpleados />} />
         </Route>
