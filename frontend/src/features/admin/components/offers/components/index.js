2edf2ed4eaee408644f55af5dc3dd78e1404ff91
Nicolas Varela <niicovarelaa@gmail.com>
2026-03-22 22:57:52 -0300

    fix: forzar mayúscula en Pagination

--- frontend/src/features/admin/components/offers/components/index.js
+++ frontend/src/features/admin/components/offers/components/index.js
@@ -1,5 +1,5 @@
 export { OfertasFilters } from "./filters/OfertasFilters";
 export { OfertasTable } from "./table";
-export { Pagination } from "./pagination/PaginationTemp";
+export { Pagination } from "./pagination/Pagination";
 export { ModalDescuento } from "./modals/ModalDescuento";
 export { ModalAgregarOferta } from "./modals/ModalAgregarOferta";
